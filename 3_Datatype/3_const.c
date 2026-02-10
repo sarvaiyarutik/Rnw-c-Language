a9d5b806796687a5ebf695e80a22e1ca5561587b
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-02-10 10:52:52 +0530

    const and scanf code added

---
+++ 3_Datatype/3_const.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+#define pi 3.14
+
+
+int main(){
+     const int num1 = 50;
+
+     printf("%d \n",num1);
+     printf("%f\n",pi);
+
+     return 0;
+}
+
---
+++ 3_Datatype/4_scanf.c
@@ -0,0 +1,15 @@
+
+
+
+#include<stdio.h>
+
+int main(){
+
+    char* scanfkeywords ;
+
+    printf("My name is ",scanfkeywords);
+
+    scanf("%s",&scanfkeywords);
+
+    return 0;
+}
