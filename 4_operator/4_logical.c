00e2a06913bdce6a42158fd09bc1cccdae6b8f1a
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-02-11 10:26:19 +0530

    new code added

---
+++ 4_operator/4_logical.c
@@ -0,0 +1,19 @@
+
+
+
+#include<stdio.h>
+
+
+int main(){
+     
+   int num1 = 200;
+
+   int num2 = 100;
+
+   printf("\nand operator: %d ", num1 && num2);
+
+   printf("\nor operator : %d", num1 || num2);
+
+   printf("\nnot operator : %d", num1 != num2);
+   return 0;
+}
---
+++ 4_operator/5_assignment.c
@@ -0,0 +1,27 @@
+
+
+
+#include<stdio.h>
+
+int main(){
+
+    int num1 = 20;
+
+
+
+
+
+
+    printf("\nadd and assign: %d", num1 += 20);
+
+    printf("\nsubtract and assign: %d", num1 -= 20);
+
+    printf("\nmultiplay and assign: %d", num1 *= 20);
+
+    printf("\ndivide and assign: %d", num1 /= 20);
+
+    printf("\n modulus and assign: %d", num1 %= 20);
+
+
+    return 0;
+}
