31a3497343fb6f8f5a3b4c44f3e67500874ff1b3
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-02-23 12:07:06 +0530

    flowchart

---
+++ 5_control_stracture/7_flowchart.c
@@ -0,0 +1,40 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+
+    int x, y, z;
+
+    printf("Enter your number:");
+    scanf("%d %d %d", &x, &y, &z);
+
+    if (x >= y)
+    {
+       if (x >= z)
+       {
+         printf("x is the largest");
+       }
+
+       else{
+        printf("z is the  largest");
+       }
+       
+    }
+
+    else{
+        if (y >= z)
+        {
+            printf("y is the largest");
+        }
+
+        else{
+            printf("z is the largest");
+        }
+        
+    }
+    
+
+    return 0;
+}
