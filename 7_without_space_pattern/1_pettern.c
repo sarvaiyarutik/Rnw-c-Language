15d20ac2ff4c43520d19bce4c556ac525c3e17b7
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-02-26 11:52:57 +0530

    new code added

---
+++ 7_without_space_pattern/1_pettern.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 5;
+
+    for (int i = 1; i <= num; i++)
+    {
+
+        for (int j = 1; j <= num; j++)
+        {
+            printf("%d", j);
+        }
+          printf("\n");
+    }
+
+  
+
+    return 0;
+}
