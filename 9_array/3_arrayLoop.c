314cbb7fdfc5d1f239247a74c1abdd81fbd09fd5
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-03-06 13:35:29 +0530

    new code added

--- 9_array/3_arrayLoop.c
+++ 9_array/3_arrayLoop.c
@@ -8,8 +8,8 @@ int main(){
     int number[] = {100,200,300,400,500};
   
 
-    for (int i = 1; i <= 5; i++){
-        printf("%d\n",i[number]);
+    for (int i = 0; i < 5; i++){
+        printf("%d\n",number[i]);
     }
 
     printf("\n");
