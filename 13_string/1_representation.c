6d9bfcb86554d825d30385c3afe677d30b2d0430
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-03-18 10:09:01 +0530

    char code added

---
+++ 13_string/1_representation.c
@@ -0,0 +1,21 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+
+    char letter1 = 'A';
+
+    printf("%c\n", letter1);
+
+    char letter2 = 'a';
+
+    printf("%c\n", letter2);
+
+    char letter3 = '0';
+
+    printf("%c\n",letter3);
+
+        return 0;
+}
